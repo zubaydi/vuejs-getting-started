8af9c36ead68d1305084740e0d955aa325a0c5b5
ahmad_zubaydi <ahmad.zubaydi@conciso.de>
2022-08-12 14:29:21 +0200

    implement productComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,15 @@
 import ProductList from "../components/ProductList.vue";
 import json from "../assets/products.json";
 import type { Product } from "@/models/Product";
+import SearchBarComponent from "../components/SearchBarComponent.vue";
 
 const products: Product[] = json;
+//const search = "" : string ; 
 </script>
 
 <template>
   <div class="home">
+    <!--SearchBarComponent @emit-execute-search="search"></SearchBarComponent-->
     <ProductList :products="products"></ProductList>
   </div>
 </template>
